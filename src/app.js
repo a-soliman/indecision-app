afd6d2da3b6e4874a8d63ea584fbd544136bf013
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-09-12 08:48:08 -0700

    updated the code using state managment

--- src/app.js
+++ src/app.js
@@ -1,15 +1,70 @@
 class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: []
+        };
+
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+        this.handlePick = this.handlePick.bind(this);
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            };
+        });
+    }
+
+    handleAddOption(option) {
+        if ( !option ) {
+            return {
+                success: false, 
+                message: 'Please provide an option to add.'
+            };
+        } 
+        if ( this.state.options.indexOf(option) !== -1 ) {
+            return {
+                success: false, 
+                message: 'Option alerdy exist'
+            };
+        }
+        this.setState((prevState) => {
+            return {
+                options: prevState.options.concat([option])
+            };
+        });
+        return {
+            success: true,
+            message: 'Option added succesfully.'
+        };
+    }
+
+    handlePick() {
+        const random = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[random];
+        console.log(option);
+    }
+
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer!';
-        const options = ['one', 'two', 'three'];
 
         return (
             <div> 
                 <Header title={title} subtitle={subtitle}/>
-                <Action />
-                <Options options={options}/>
-                <AddOption />
+                <Action 
+                    optionsCount = {this.state.options.length > 0}
+                    options = {this.state.options}
+                    handlePick = {this.handlePick}
+                    />
+                <Options 
+                    options={this.state.options} 
+                    handleDeleteOptions={this.handleDeleteOptions}
+                />
+                <AddOption handleAddOption={this.handleAddOption}/>
             </div>
         );
     }
@@ -30,7 +85,12 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button 
+                    onClick = {this.props.handlePick}
+                    disabled={!this.props.optionsCount}
+                >
+                    What should I do?
+                </button>
             </div>
         );
     }
@@ -40,8 +100,13 @@ class Options extends React.Component {
     render() {
         return (
             <div>
+                <button 
+                    onClick={this.props.handleDeleteOptions}
+                    disabled={!this.props.options.length > 0}
+                >
+                    Remove all
+                </button>
                 <p>Options Component</p>
-                <p>{this.props.options.length}</p>
                 {
                     this.props.options.map( (option, i) => <Option key={i} content={option} /> )
                 }
@@ -61,13 +126,60 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: undefined,
+            message: ''
+        };
+
+        this.submit = this.submit.bind(this);
+        this.clearMessage = this.clearMessage.bind(this);
+    }
+    submit(e) {
+        e.preventDefault();
+
+        const valueToAdd    = e.target.elements.option.value.trim();
+        const response      = this.props.handleAddOption(valueToAdd);
+
+        if ( response.success ) {
+            this.setState(() => {
+                return { error: undefined, message: response.message };
+            });
+            e.target.elements.option.value = '';
+        } 
+        else {
+            this.setState(() => {
+                return { error: true, message: response.message };
+            });
+        }
+    }
+
+    clearMessage() {
+        this.setState(() => {
+            return {
+                message: ''
+            };
+        });
+    }
+
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" name="option" />
+                <form onSubmit={this.submit}>
+                    <input type="text" name="option" onChange={this.clearMessage} />
                     <button>Add Option</button>
                 </form>
+                {this.state.error && 
+                    <div className="alert alert-danger">
+                        {this.state.message}
+                    </div>
+                }
+                {!this.state.error && 
+                    <div className="alert alert-success">
+                        {this.state.message}
+                    </div>
+                }
             </div>
         );
     }
