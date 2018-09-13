f402518f2a1e7e9b2881e91a76c8d8c911c2baea
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-09-13 07:38:31 -0700

    using default props for more flexible component

--- src/app.js
+++ src/app.js
@@ -74,10 +74,14 @@ const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h3>{props.subtitle}</h3>
+            {props.subtitle && <h3>{props.subtitle}</h3>}
         </div>
     );
 }
+
+Header.defaultProps = {
+    title: 'Some default'
+};
 
 const Action = (props) => {
     return (
