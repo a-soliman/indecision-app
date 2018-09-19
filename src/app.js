8a9d1e95d23f278b965cfcca40d9eaa7a216d692
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-09-19 09:07:39 -0700

    importing normalize CSS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize-scss/sass/_normalize.scss';
 import './styles/styles.scss';
-
 
 const appContainer = document.querySelector('#app');
 ReactDOM.render(<IndecisionApp />, appContainer);
